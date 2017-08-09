622a6e423d701506b5ae442a43c780c4e64beeee
John Ryan Gisel <johnryangisel@Johns-MacBook-Pro.local>
2017-08-09 07:37:35 -0700

    initial commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by John Ryan Gisel on 8/9/17.
+//  Copyright © 2017 Jack GIsel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
