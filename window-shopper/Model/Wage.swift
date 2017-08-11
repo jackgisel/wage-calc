5ef646e1639b3b49c895df339a1eaacf0a64d47f
John Ryan Gisel <johnryangisel@Johns-MacBook-Pro.local>
2017-08-11 09:16:31 -0700

    Added getHours function and unit testing

---
+++ window-shopper/Model/Wage.swift
@@ -0,0 +1,15 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by John Ryan Gisel on 8/11/17.
+//  Copyright © 2017 Jack GIsel. All rights reserved.
+//
+
+import Foundation
+class wage {
+    class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        
+        return Int(ceil(price / wage))
+    }
+}
---
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,38 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by John Ryan Gisel on 8/9/17.
+//  Copyright © 2017 Jack GIsel. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTxtField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+
+    func customizeView(){
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = " "
+        }
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
